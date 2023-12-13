1f0767260794def9c9d30daec8c8de0838b0d4bf
jasur sadullaev <jasur.sadullayev@stylemix.net>
2023-12-13 10:07:31 +0800

    added pie chart

--- index.php
+++ index.php
@@ -5,28 +5,45 @@ require 'class-GalaxyApps.php';
 $galaxy_apps = new GalaxyApps();
 
 ?>
-<div>
-	<canvas id="myChart"></canvas>
+<div class="wrapper" style="max-width: 800px">
+	<canvas id="firstChart" width="800" height="500" aria-label="First chart"></canvas>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script>
-	const ctx = document.getElementById('myChart');
+	// Following scripts are for demonstration
+	const ctx = document.getElementById('firstChart');
 	let apps = <?php $galaxy_apps::get_apps(); ?>;
 	let categories = [];
+	let ratings = [];
 	apps.forEach( function( app ) {
 		categories.push(app.Category);
+		ratings.push(app.Rating);
 	});
+
+	categories = removeDuplicates(categories)
+	ratings = removeDuplicates(ratings)
+
 	new Chart(
 		ctx,
 		{
-			type: 'bar',
+			type: 'pie',
 			data: {
-				labels: [...new Set(categories)],
+				labels: categories,
 				datasets: [{
-					label: 'Category',
-					data: apps.forEach((app) => app.Rating)
-				}]
+					label: 'Average Rating',
+					data: ratings,
+				},]
 			},
 	});
+
+	function removeDuplicates( data ) {
+		let unique = [];
+		data.forEach( function( element ) {
+			if( !unique.includes(element) ) {
+				unique.push(element)
+			}
+		});
+		return unique;
+	}
 </script>
